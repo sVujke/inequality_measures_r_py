612f40ae0566f6ac05cf1d73e9f6be6994c09f40
sVujke <stefanvujovic93@gmail.com>
2017-05-04 18:02:18 +0200

    lib added

---
+++ demo.R
@@ -0,0 +1,22 @@
+library(ineq)
+data_set <- read.csv('users_dataset.csv', TRUE, ",")
+head(data_set)
+income <- data_set$income
+paste("gini:", ineq(income,type = "Gini"), sep=" ")
+#print("gini: ",ineq(data_set$income,type = "Gini")
+#plot(Lc(AirPassengers))
+
+gini <- ineq(income,type = "Gini")
+atkinson0 <- Atkinson(income, parameter = 0, na.rm = TRUE)
+atkinson05 <- Atkinson(income, parameter = 0.5, na.rm = TRUE)
+atkinson1 <- Atkinson(income, parameter = 1, na.rm = TRUE)
+thiel <- Theil(income, parameter = 0, na.rm = TRUE)
+var_coef <- var.coeff(income)
+kolm <- Kolm(income, parameter = 1, na.rm = TRUE)
+
+paste("Gini: ", gini)
+paste("Atkinson epsilon=0: ", atkinson0)
+paste("Atkinson epsilon=0.5: ", atkinson05)
+paste("Atkinson epsilon=1: ", atkinson1)
+paste("Thiel: ", thiel)
+paste("Coef of variation: ", var_coef)
